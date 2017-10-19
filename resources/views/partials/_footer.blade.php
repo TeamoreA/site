5d499410d6f95af0eef6525ad80cfb0c3cff65ab
Teamo <teamorekamau97@gmail.com>
2017-10-19 17:41:58 +0300

    Footer Modified

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,3 +1,4 @@
       <hr>
 
-      <p class="text-center col-lg-12" >Copyright &copy; Teamore - All Rights Reserved 2017</p>
+      <p class="text-center col-lg-12" >Copyright &copy; Teamore - All Rights Reserved 2017
+      A product of Developer~X Company</p>
